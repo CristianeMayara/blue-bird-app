ce3c7e1c14d1197432ede414062f0d01a76876fb
Cristiane Mayara <cris.mayara@gmail.com>
2019-02-10 22:36:37 -0200

    Store username from login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,12 +4,34 @@ import twitterLogo from '../twitter.svg';
 import './Login.css';
 
 export default class Login extends Component {
+  state = {
+    username: ''
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { username } = this.state;
+    if (!username.length) return;
+
+    localStorage.setItem('@MyTwitter:username', username);
+
+    this.props.history.push('/timeline');
+  }
+
+  handleInputChange = e => {
+    this.setState({ username: e.target.value });
+  }
+
   render() {
     return (
       <div className="login-wrapper">
         <img src={twitterLogo} alt="MyTwitter" />
-        <form>
-          <input placeholder="Usuário" />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            placeholder="Usuário"
+            value={this.state.username}
+            onChange={this.handleInputChange} />
           <button type="submit">Entrar</button>
         </form>
       </div>
